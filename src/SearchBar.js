70bbda09fca2825d95878f1002c5b0e293c5ad9b
abbasali1997 <64266126+abbasali1997@users.noreply.github.com>
2020-12-14 18:26:26 +0500

    Update SearchBar.js

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -23,11 +23,16 @@ class SearchBar extends Component {
 		this.setState({search: ''});
 	}
 	render() {
+		const btnContent = (this.props.load) ? 
+		(<div className="spinner-border spinner-border-sm text-light" role="status">
+			<span className="visually-hidden">Loading...</span>
+		</div>)
+		: <i className="fas fa-search"></i>;
 		return (
-			<form className="w-25 mx-auto d-flex justify-content-center align-items-start" onSubmit={this.handleSubmit}>
-			  <div className="form-group">
+			<form className="w-25 mx-auto row" onSubmit={this.handleSubmit}>
+			  <div className="form-group col-8">
 				  <input type="text" 
-					     className="form-control w-auto" 
+					     className="form-control" 
 					     id="search" 
 					     name="search" 
 					     placeholder="What are you looking for?" 
@@ -36,7 +41,7 @@ class SearchBar extends Component {
 				  	     onChange= {this.handleChange}></input>
 			  </div>
 			  <button type="submit" 
-				      className="btn btn-primary ml-3">Search</button>  	
+				      className="btn btn-success col-4">{btnContent}</button>  	
 			</form>
 		)	
 	}
