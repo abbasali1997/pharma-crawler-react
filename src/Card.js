0762c5c7e5e9d2a85d5da61aa2b2036ea6f5fcb0
abbasali1997 <64266126+abbasali1997@users.noreply.github.com>
2020-12-14 18:24:45 +0500

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css';
 
-const Card = ({ title, image, price, formula, company }) => {
+const Card = ({ title, image, price, formula, company, site, type }) => {
 	return (
 		
 		<div className="col-12 col-sm-6 col-md-4 col-xl-3 my-3">
@@ -13,14 +13,20 @@ const Card = ({ title, image, price, formula, company }) => {
 				/>
 				<div className="card-body">
 					<h5 className="card-title productName">{title}</h5>
-					<div className="text-center mb-1">
-						<b className="card-text d-block">{price}</b>
+					<div className="text-center my-2">
+						<b className="card-text d-block" style={{color: 'deeppink'}}>{price}</b>
 					</div>
-					<div className="text-left">
-						<b>Mfr. </b> <span style={{color: 'blue'}}>{company}</span>
+					<div className="text-start">
+						<b>Type. </b> {type}
 					</div>
-					<div className="text-left">
+					<div className="text-start">
+						<b>Mfr. </b> {company}
+					</div>
+					<div className="text-start">
 						<b>formula. </b> {formula}
+					</div>
+					<div className="text-start">
+						<b>Site. </b> <a href={site}>{site}</a>
 					</div>
 				</div>
 			</div>
